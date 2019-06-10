b3359c468ea5804c79ad6850d1f11fe7bbcfaf7d
Mirko <mirkoghisla01@gmail.com>
2019-06-10 17:44:36 +0200

    Il Personaggio si muove!
    SI CAZZO DIOMERDAAAAA

---
+++ Picchiaduro/Assets/Script/collisione.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisione : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.GetType().ToString() == "UnityEngine.SphereCollider")
+        {
+            Debug.Log("AIUTO SONO MORTO");
+        }
+    }
+}
---
+++ Picchiaduro/Assets/Script/comandi.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comandi : MonoBehaviour
+{
+
+    Animator anim;
+    int pugno = Animator.StringToHash("pugno");
+    int calcio = Animator.StringToHash("calcio");
+    int salto = Animator.StringToHash("salto");
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+                transform.Translate(0, 0, +0.7f);
+            
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(0, 0, -0.7f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+            anim.SetTrigger(pugno);
+
+        if (Input.GetKeyDown(KeyCode.X))
+            anim.SetTrigger(calcio);
+
+        if (Input.GetKeyDown(KeyCode.W))
+            anim.SetTrigger(salto);
+
+    }
+
+
+
+}
