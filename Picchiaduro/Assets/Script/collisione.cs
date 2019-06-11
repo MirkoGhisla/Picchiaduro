3b95a86b98cee66ba53dc36059941f4c53703468
Mirko <mirkoghisla01@gmail.com>
2019-06-11 12:36:31 +0200

    Barra vita
    Aggiunta barra vita (max 1000) con danno base 100

--- Picchiaduro/Assets/Script/collisione.cs
+++ Picchiaduro/Assets/Script/collisione.cs
@@ -1,15 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class collisione : MonoBehaviour
 {
+
+    public int startingHealth = 1000;                            
+    public int currentHealth;                                   
+    public Slider healthSlider;
+
+    private void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
     void OnCollisionEnter(Collision collision)
     {
 
         if (collision.collider.GetType().ToString() == "UnityEngine.SphereCollider")
         {
-            Debug.Log("AIUTO SONO MORTO");
+
+  
+            currentHealth -= 100;
+
+
+            healthSlider.value = currentHealth;
+
+        
         }
 
     }
